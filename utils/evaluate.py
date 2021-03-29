f45107f5e574a1d006302db6a1e3cc4597502e3f
kalimali <kirtanmali555@gmail.com>
2021-03-29 23:24:53 +0530

    original evaluate.py

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -6,8 +6,7 @@ import torch
 import torch.nn as nn
 import json
 from utils.wandb_utils import wandb_log_conf_matrix
-from sklearn.metrics import f1_score, multilabel_confusion_matrix, recall_score, precision_score, classification_report, confusion_matrix
-import json
+from sklearn.metrics import f1_score, multilabel_confusion_matrix, recall_score, precision_score, classification_report
 
 
 def calc_acc_n_loss(args, model, loader, log_matrix=False):
@@ -49,63 +48,7 @@ def calc_acc_n_loss(args, model, loader, log_matrix=False):
     precision=precision_score(y_true,y_pred,average='weighted')
     recall=recall_score(y_true,y_pred,average='weighted')
 
-    mat = confusion_matrix(y_true, y_pred)
-    accs = mat.diagonal()/mat.sum(axis=1)
-    correct = mat.diagonal()
-
-    heatmap = [ {'x': j, 'y': i, 'heat': f'{mat[i][j]:.03f}'} for i in range(len(mat)) for j in range(len(mat[0])) ]
-    # print(heatmap)
-
-    string = json.dumps(heatmap)
-
-    with open('save_mat.json', 'w') as f:
-        f.write(f"data = '{string}'")
-    # np.save('confusion_matrix', np.array(mat))
-
-    print('accuracy')
-    for i in accs:
-        print(i)
-    print('correct')
-    for i in correct:
-        print(i)
-
-    report = (classification_report(y_true, y_pred, output_dict=True))
     print(classification_report(y_true, y_pred))
-    file = open('metrics.json', 'w')
-    file.write(json.dumps(report, indent=4))
-    # report = (classification_report(y_true, y_pred))
-    # print(report)
-    print(report)
-    metric = 'support'
-    print(metric)
-    for k, v in report.items():
-        try:
-            print(f'{v[metric]:.2f}')
-        except:
-            pass
-    metric = 'precision'
-    print(metric)
-    for k, v in report.items():
-        try:
-            print(f'{v[metric]:.2f}')
-        except:
-            pass
-
-    metric = 'recall'
-    print(metric)
-    for k, v in report.items():
-        try:
-            print(f'{v[metric]:.2f}')
-        except:
-            pass
-
-    metric = 'f1-score'
-    print(metric)
-    for k, v in report.items():
-        try:
-            print(f'{v[metric]:.2f}')
-        except:
-            pass
 
     if log_matrix == True:
         wandb_log_conf_matrix(y_true, y_pred)
